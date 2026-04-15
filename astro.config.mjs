0430d12fc62fe5481ad7da5e8a9880cc45222503
At Home <53349939+leveler85@users.noreply.github.com>
2026-04-15 21:47:10 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://kpsfamily.github.io",
-  base: "/youtube-briefing",
+  base: "/youtube",
   trailingSlash: "ignore",
   build: {
     format: "directory",
